f1e6bcb029fc17e5231e5d1c596c41a5f29b85f8
othman <acerxr@gmail.com>
2024-04-20 02:18:51 +0300

    Update join game view

---
+++ src/main/java/game/code/Server.java
@@ -0,0 +1,8 @@
+package game.code;
+
+public class Server {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
--- src/main/java/game/views/join/JoinView.java
+++ src/main/java/game/views/join/JoinView.java
@@ -4,6 +4,8 @@ import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -18,52 +20,70 @@ public class JoinView extends HorizontalLayout {
 
 
     public JoinView() {
-        // MAIN LAYOUT
-        addClassName("join-view");
+            // Center the content
+            setAlignItems(Alignment.CENTER);
+            addClassName("join-view");
 
-        Div mainContainer = new Div();
-        mainContainer.addClassName("main-container");
+            // Main container for the join view
+            Div joinContainer = new Div();
+            joinContainer.addClassName("join-container");
 
-        // Create a title for the view
-        H2 title = new H2("Join Game");
-        title.addClassName("join-title");
+            // Header Icon for the join container
+            Image headerIcon = new Image("https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Neutral%20Face.png", "Join Icon");
+            headerIcon.addClassName("header-icon");
+            joinContainer.add(headerIcon);
+
+            // Add a header to the join container
+            H2 joinHeader = new H2("Join a Game");
+            joinHeader.addClassName("join-header");
+            joinContainer.add(joinHeader);
+
+            // Create a text field for the game code
+            TextField gameCodeField = new TextField();
+            gameCodeField.setPlaceholder("Enter Game Name");
+            gameCodeField.addClassName("game-name-field");
+            joinContainer.add(gameCodeField);
+
+            // Buttons container
+            HorizontalLayout buttonsContainer = new HorizontalLayout();
+            buttonsContainer.addClassName("buttons-container");
+
+            // Create a buttons to join or create a game
+            Button joinButton = new Button("Join Game");
+            joinButton.addClassName("join-button");
+            // add icon to the button
+            Icon arrowRightIcon = new Icon("lumo", "arrow-right");
+            arrowRightIcon.addClassName("arrow-icon");
+            joinButton.setIcon(arrowRightIcon);
+            joinButton.setIconAfterText(true);
+            joinButton.addClickShortcut(Key.ENTER);
+            joinButton.addClickListener(e -> {
+                Notification.show("Joining game with code: " + gameCodeField.getValue());
+            });
+
+            Button createButton = new Button("Create Game");
+            createButton.addClassName("create-button");
+            // add icon to the button
+            Icon plusIcon = new Icon("lumo", "plus");
+            plusIcon.addClassName("plus-icon");
+            createButton.setIcon(plusIcon);
+            createButton.setIconAfterText(true);
+
+            createButton.addClickListener(e -> {
+                Notification.show("Creating game with name: " + gameCodeField.getValue());
+            });
+
+            // Add the buttons to the buttons container
+            buttonsContainer.add(joinButton, createButton);
+
+            // Add the buttons container to the join container
+            joinContainer.add(buttonsContainer);
 
 
-        // Create a text field for the player's name
-        TextField nameField = new TextField("Name");
-        nameField.setPlaceholder("Enter your name");
-        nameField.setClassName("name-field");
-        nameField.setClearButtonVisible(true);
-        nameField.focus();
-
-        // Create a button to join the game
-        Div buttonContainer = new Div();
-        buttonContainer.addClassName("button-container");
-        Button joinButton = new Button("Join");
-        joinButton.setThemeName("primary");
-        joinButton.setClassName("join-btn");
-        joinButton.addClickShortcut(Key.ENTER);
-        joinButton.addClickListener(e -> {
-            Notification.show("Game joined!");
-            // Go to the join game view
-            Notification.show("Game joined!");
-        });
-        Button createGameButton = new Button("Create Game");
-        createGameButton.setThemeName("primary");
-        createGameButton.setClassName("create-btn");
-        createGameButton.addClickShortcut(Key.ENTER);
-        createGameButton.addClickListener(e -> {
-            Notification.show("Game created!");
-            // Go to the join game view
-            Notification.show("Game created!");
-        });
-
-        buttonContainer.add(joinButton, createGameButton);
+            // Add the join container to the view
+            add(joinContainer);
 
 
-        mainContainer.add(title, nameField, buttonContainer);
-        // Add the components to the layout
-        add(mainContainer);
     }
 
 }
