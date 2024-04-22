04a21d092d0f432baed38741118098f0787be4b3
othman <acerxr@gmail.com>
2024-04-22 14:22:56 +0300

    Add dynamic routing

--- src/main/java/game/views/games/GamesView.java
+++ src/main/java/game/views/games/GamesView.java
@@ -1,15 +1,17 @@
 package game.views.games;
 
-import com.vaadin.flow.component.Html;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import game.code.Game;
+import com.vaadin.flow.server.VaadinService;
 import game.views.MainLayout;
-import com.vaadin.flow.component.button.Button;
+
+import java.util.Arrays;
 
 @PageTitle("Games")
 @Route(value = "games", layout = MainLayout.class)
@@ -18,7 +20,6 @@ public class GamesView extends VerticalLayout {
     public GamesView() {
         // Center the content
         setAlignItems(Alignment.CENTER);
-
 
         // Create a container for the games and add a header
         Div gamesContainer = new Div();
@@ -34,41 +35,82 @@ public class GamesView extends VerticalLayout {
         gameCardsContainer.addClassName("game-cards-container");
         gamesContainer.add(gameCardsContainer);
 
-
-        Div gameCard1 = createGameCard();
-        Div gameCard2 = createGameCard();
-        Div gameCard3 = createGameCard();
-        Div gameCard4 = createGameCard();
-        Div gameCard5 = createGameCard();
-
-        gameCardsContainer.add(gameCard1, gameCard2, gameCard3, gameCard4, gameCard5);
+        for (int i = 1; i <= 5; i++) {
+            Div gameCard = createGameCard("Game " + i);
+            gameCardsContainer.add(gameCard);
+        }
 
         add(gamesContainer);
-
     }
 
-    public Div createGameCard() {
+    public Div createGameCard(String gameName) {
         Div gameCard = new Div();
 
         // Here we will create a card for the game
         gameCard.addClassName("game-card");
 
-        H3 gameTitle = new H3("Game Name");
+        H1 gameTitle = new H1(gameName);
         gameCard.add(gameTitle);
 
-        Html joinButton = new Html("<button class=\"button\">Join</button>");
+        Button joinButton = new Button("Join");
+        joinButton.addClickListener(e -> {
+            Notification.show("Joined the game " + gameName);
+            UI.getCurrent().navigate("games/" + gameName.replaceAll(" ", "%20"));
+        });
         gameCard.add(joinButton);
-
-        // // Set the game name as the card title
-        // Html gameTitle = new Html("<h3>" + "Game Name" + "</h3>");  
-        // gameCard.add(gameTitle);
-
-        // // Add a button to join the game
-        // Html joinButton = new Html("<button>Join Game</button>");
-        // gameCard.add(joinButton);
-        
 
         return gameCard;
     }
 
+    @Route(value = "games/:gameName", layout = MainLayout.class)
+    public static class GameView extends VerticalLayout {
+
+        public GameView() {
+            // Get the game ID from the URL
+            String gameName = this.getGameName();
+            Notification.show("Game Name: " + gameName);
+
+            // Center the content
+            setAlignItems(Alignment.CENTER);
+
+            // Create a container for the game and add a header
+            Div gameContainer = new Div();
+
+            // Add a header to the game container
+            H1 gameHeader = new H1("Game " + gameName);
+
+            // Add the game header to the game container
+            gameContainer.add(gameHeader);
+
+            // Add the game container to the view
+            add(gameContainer);
+
+            // Add a button to start the game
+            Button startGameButton = new Button("Start Game");
+            startGameButton.addClickListener(e -> {
+                // Redirect to the game view
+                Notification.show("Game started!");
+            });
+
+            // Add the start game button to the view
+            add(startGameButton);
+
+            // Add a button to go back to the games list
+            Button backButton = new Button("Back to Games List");
+            backButton.addClickListener(e -> {
+                // Redirect to the games view
+                getUI().ifPresent(ui -> ui.navigate("games"));
+            });
+
+            // Add the back button to the view
+            add(backButton);
+        }
+
+        private String getGameName() {
+            String pathInfo = VaadinService.getCurrentRequest().getPathInfo();
+            String[] parts = pathInfo.split("/");
+            return Arrays.stream(parts).reduce((first, second) -> second).orElse("");
+
+        }
+    }
 }
